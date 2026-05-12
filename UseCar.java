57a591460459b50e7d1e37e236d37eddc66874d4
t1ww <95153968+t1ww@users.noreply.github.com>
2026-05-12 22:53:31 +0700

    feat: Checking speed and hit points

--- UseCar.java
+++ UseCar.java
@@ -8,15 +8,16 @@ public class UseCar {
 
         Scanner scan = new Scanner(System.in);
         while (true) {
-            System.out.println("Enter command ((accel)accelerate, brake, damage, exit): ");
+            System.out.println("Enter command (accel(accelerate), brake, damage, exit, or");
+            System.out.println("checkspeed, checkhp): ");
             String command = scan.nextLine();
 
             if (command.equalsIgnoreCase("accelerate") || command.equalsIgnoreCase("accel")) {
                 car.accelerate();
-                System.out.println("Car speed: " + car.getSpeed());
+
             } else if (command.equalsIgnoreCase("brake")) {
                 car.brake();
-                System.out.println("Car speed: " + car.getSpeed());
+
             } else if (command.equalsIgnoreCase("damage")) {
                 Car damagedCar = car.takeDamage();
                 if (damagedCar == null) {
@@ -25,9 +26,18 @@ public class UseCar {
                 } else {
                     System.out.println("Car hit points: " + damagedCar.getHitPoints());
                 }
+
             } else if (command.equalsIgnoreCase("exit")) {
                 System.out.println("Exiting...");
                 break;
+
+            } else if (command.equalsIgnoreCase("checkspeed")) {
+                // Checking speed and hit points commands to show the use of getter methods.
+                System.out.println("Car speed: " + car.getSpeed());
+
+            } else if (command.equalsIgnoreCase("checkhp")) {
+                System.out.println("Car hit points: " + car.getHitPoints());
+
             } else {
                 System.out.println("Invalid command. Please try again.");
             }
