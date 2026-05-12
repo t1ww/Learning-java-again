a5601e208843ecdb9de8d756698a4f107c9596c5
t1ww <95153968+t1ww@users.noreply.github.com>
2026-05-12 22:44:21 +0700

    commit 2: full program

--- OurObject/Car.java
+++ OurObject/Car.java
@@ -5,7 +5,7 @@ public class Car {
     private int hitPoints = 2;
     private int accelerationSpeed = 20;
     private int speed = 0;
-    private int maxSpeed = 200;
+    private int maxSpeed = 100;
 
     // Constructor to initialize the self reference
     public Car() {
@@ -15,22 +15,35 @@ public class Car {
     public void accelerate() {
         if (speed < maxSpeed) {
             speed += accelerationSpeed;
+        } else {
+            System.out.println("YOU ARE TOO FAST! CANT SPEED UP ANYMORE!");
         }
     }
 
     public void brake() {
         if (speed > 0) {
             speed -= accelerationSpeed;
+        } else {
+            System.out.println("You've come to stopped, do something else.");
         }
+    }
+
+    private void tryDying() {
+        if (hitPoints <= 0) {
+            die();
+        }
+    }
+
+    private void die() {
+        // Return the car itself if still alive, otherwise null
+        System.out.println("Car is destroyed!");
+        self = null; // Simulate the car being destroyed
     }
 
     public Car takeDamage() {
         hitPoints--;
-        if (hitPoints <= 0) {
-            // Return the car itself if still alive, otherwise null
-            System.out.println("Car is destroyed!");
-            self = null; // Simulate the car being destroyed
-        }
+        // The code now easier to tell we're checking if we dies after taking damage, and we can also easily change the code to do something else before dying if we want to.
+        tryDying();
         return self;
     }
 
--- UseCar.java
+++ UseCar.java
@@ -1,8 +1,37 @@
 import OurObject.Car;
+import java.util.Scanner;
 
 public class UseCar {
     public static void main(String[] args) {
         Car car = new Car();
         System.out.println("Car speed: " + car.getSpeed());
+
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter command (accelerate, brake, damage, exit): ");
+            String command = scan.nextLine();
+
+            if (command.equalsIgnoreCase("accelerate")) {
+                car.accelerate();
+                System.out.println("Car speed: " + car.getSpeed());
+            } else if (command.equalsIgnoreCase("brake")) {
+                car.brake();
+                System.out.println("Car speed: " + car.getSpeed());
+            } else if (command.equalsIgnoreCase("damage")) {
+                Car damagedCar = car.takeDamage();
+                if (damagedCar == null) {
+                    System.out.println("The car is destroyed and can no longer be used.");
+                    break;
+                } else {
+                    System.out.println("Car hit points: " + damagedCar.getHitPoints());
+                }
+            } else if (command.equalsIgnoreCase("exit")) {
+                System.out.println("Exiting...");
+                break;
+            } else {
+                System.out.println("Invalid command. Please try again.");
+            }
+        }
+        scan.close();
     }
 }
